8a398beadf3244488462fb1341de573b2340dbe2
Adam Holt <adam.holt@mrm-mccann.com>
2019-10-01 11:34:51 -0600

    Updated to support needs of cert installs in other docker images.

--- src/Utilities/Sannel.House.Utilities.Certs/Program.cs
+++ src/Utilities/Sannel.House.Utilities.Certs/Program.cs
@@ -20,16 +20,45 @@ namespace Sannel.House.Utilities.Certs
 
 				var log = provider.GetService<ILogger<Program>>();
 
-				if (args.Length < 1)
+				if (args.Length < 2)
 				{
-					log.LogCritical("You must pass in the path to the crt");
+					log.LogCritical("You must pass in the path to the crt and the store location i.e. (c == current user my, s == local computer my, r == local computer trusted root authority");
 					return;
 				}
 
 				log.LogInformation("Trying to install {0}", args[0]);
 
-				IServiceProviderExtensions.InstallCertificate(System.Security.Cryptography.X509Certificates.StoreName.AuthRoot,
-				System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine, args[0], log);
+				string password = null;
+
+				if(args.Length >= 2 && !string.IsNullOrEmpty(args[2]))
+				{
+					password = args[2];
+				}
+
+				if(args[1] == "c")
+				{
+					log.LogInformation("Current User My");
+					IServiceProviderExtensions.InstallCertificate(System.Security.Cryptography.X509Certificates.StoreName.My, System.Security.Cryptography.X509Certificates.StoreLocation.CurrentUser, args[0], log, password);
+				}
+				else if(args[1] == "cr")
+				{
+					log.LogInformation("Current User Auth Root");
+					IServiceProviderExtensions.InstallCertificate(System.Security.Cryptography.X509Certificates.StoreName.AuthRoot, System.Security.Cryptography.X509Certificates.StoreLocation.CurrentUser, args[0], log, password);
+				}
+				else if(args[1] == "s")
+				{
+					log.LogInformation("System My");
+					IServiceProviderExtensions.InstallCertificate(System.Security.Cryptography.X509Certificates.StoreName.My, System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine, args[0], log, password);
+				}
+				else if(args[1] == "r")
+				{
+					log.LogInformation("System Auth Root");
+					IServiceProviderExtensions.InstallCertificate(System.Security.Cryptography.X509Certificates.StoreName.AuthRoot, System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine, args[0], log, password);
+				}
+				else
+				{
+					log.LogInformation("Invalid argument passed {0}", args[1]);
+				}
 			}
 		}
 	}
